8d14ce0416ca2c2ff720674b5cb5ed1daccc3f58
fYmious <132186293+fYmious@users.noreply.github.com>
2023-05-04 20:37:01 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-f = open('26.txt')
-n = f.readline()
-cubes = sorted([int(i) for i in f], reverse=True)
-cklad = []
-while len(cubes) > 0:
-    block = [cubes.pop(0)]
-    for i in range(len(cubes)):
-        if (block[-1] - cubes[i]) >= 5:
-            block.append(cubes[i])
-            cubes[i] = ''
-    cklad.append(block)
-    cubes = [x for x in cubes if x != '']
-print(len(cklad), max(len(c) for c in cklad))
+free_space = int(((open('27886.txt').readlines())[0].split())[0])
+data = sorted([int(i) for i in (open('27886.txt').readlines())[1:]])
+counter = 0
+for count in range(len(data)):
+    if counter + data[count] > free_space: break
+    counter += data[count]
+print(count)
+print(data[count] + (free_space - counter) if (data[count] + (free_space - counter)) in data[count:] else '')
